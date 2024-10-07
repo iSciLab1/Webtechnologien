e12cb2e6064ad1e36bf644bd37256a5ac62edf30
Mohammad Javad Imani <mohammad.imani@stud.hawk.de>
2024-10-07 17:26:48 +0200

    feat: add project data including [.html, .js, .css]

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Webtechnologien
+# ------------------------------------------------------------------------------#
+# Project Name:       Webtechnologien                                           #
+# Author:             J. Imani - iSciLab                                        #
+# Description:        A collection of experiments and personal trials in coding.#
+# ------------------------------------------------------------------------------#
---
+++ script.js
@@ -0,0 +1,9 @@
+/* -----------------------------------------------------------------------------#
+# Program Name:       [script.js]                                               #
+# Author:             J. Imani - iSciLab                                        #
+# Description:        A collection of experiments and personal trials in coding.#
+# ------------------------------------------------------------------------------#
+*/
+function displayMessage() {
+    alert("Hallo, dies ist eine interaktive Nachricht!");
+}
